8870074e37904ccb03375e366d42c34953f1fc29
vko-rx <amhdksalsgnl@gmail.com>
2023-05-25 18:50:24 +0900

    4-4 직접 풀어보기

--- app/src/main/java/kr/hs/emirim/kjminn/petphoto/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/kjminn/petphoto/MainActivity.kt
@@ -3,14 +3,22 @@ package kr.hs.emirim.kjminn.petphoto
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.*
+import android.view.View.OnClickListener
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.RadioGroup
+import android.widget.Switch
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    lateinit var checkStart:CheckBox
+    lateinit var checkStart:Switch
     lateinit var rg:RadioGroup
     lateinit var linear:LinearLayout
-    lateinit var btnDone:Button
     lateinit var imgv:ImageView
+    lateinit var btnFinish:Button
+    lateinit var btnFirst :Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +26,9 @@ class MainActivity : AppCompatActivity() {
         checkStart = findViewById(R.id.check_start)
         rg = findViewById(R.id.rg)
         linear = findViewById(R.id.linear)
-        btnDone = findViewById(R.id.btn_done)
         imgv = findViewById(R.id.imgv)
+        btnFinish = findViewById(R.id.btn_finish)
+        btnFirst = findViewById(R.id.btn_first)
         linear.visibility = View.INVISIBLE
 
         checkStart.setOnCheckedChangeListener{compoundButton, b ->
@@ -29,14 +38,32 @@ class MainActivity : AppCompatActivity() {
                 linear.visibility = View.INVISIBLE
             }
         }
-        btnDone.setOnClickListener{
-            when(rg.checkedRadioButtonId){
-                R.id.radio_dog->imgv.setImageResource(R.drawable.dog)
-                R.id.radio_cat->imgv.setImageResource(R.drawable.cat)
-                R.id.radio_rabbit->imgv.setImageResource(R.drawable.rabbit)
-                else -> Toast.makeText(applicationContext, "라디오 버튼이 선택되지 않았습니다", Toast.LENGTH_SHORT).show()
 
+        rg.setOnCheckedChangeListener{compoundButton, b ->
+            when(rg.checkedRadioButtonId){
+                R.id.radio_dog -> imgv.setImageResource(R.drawable.dog)
+                R.id.radio_cat -> imgv.setImageResource(R.drawable.cat)
+                R.id.radio_rabbit -> imgv.setImageResource(R.drawable.rabbit)
             }
+        }
+
+        btnFinish.setOnClickListener(btnListener)
+        btnFirst.setOnClickListener(btnListener)
+    }
+
+    var btnListener = OnClickListener{
+        when(it.id){
+            R.id.btn_finish -> finish()
+            R.id.btn_first -> {
+                linear.visibility = View.INVISIBLE
+                checkStart.isChecked = false
+            }
+
         }
     }
 }
+
+
+
+
+
